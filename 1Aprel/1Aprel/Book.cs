79bf4f317ed600130be4ddabe724d449814670e9
Elyane <elyamehiyeva@gmail.com>
2022-04-01 10:12:46 +0400

    1 Aprel

---
+++ 1Aprel/1Aprel/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1Aprel
+{
+    internal class Book
+    {
+        private static int _no;
+        public Book()
+        {
+            _no++;
+            this.No = _no;
+        }
+        public int No { get; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+    }
+}
---
+++ 1Aprel/1Aprel/Library.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1Aprel
+{
+    internal class Library
+    {
+        List<Book> Books = new List<Book>();
+        public List<Book> FindAllBooksByName(string value)
+        {
+            List<Book> FilteredBooks = new List<Book>();
+            foreach (Book book in Books)
+            {
+                FilteredBooks.Add(book);
+            }
+            return FilteredBooks;
+        }
+        public void RemoveAllBookByName(string value)
+        {
+            foreach (Book book in Books)
+            {
+                if(book.Name.Contains(value))
+                {
+                    Books.Remove(book);
+                }
+            }
+            
+        }
+        public List<Book> SearchBooks(string value)
+        {
+            List<Book> newbooks = new List<Book>();
+            foreach (Book book in Books)
+            {
+                string text = book.Name + book.AuthorName + book.PageCount;
+                if (text.Contains(value))
+                {
+                    newbooks.Add(book);
+                }
+            }
+            return newbooks;
+        }
+        public List<Book> FindAllBooksByPageCountRange(int minCount, int maxCount)
+        {
+            List<Book> FilteredForPageCount = new List<Book>();
+            foreach(Book book in Books)
+            {
+                if (book.PageCount>minCount && book.PageCount<maxCount)
+                {
+                    FilteredForPageCount.Add(book);
+                }
+            }
+            return FilteredForPageCount;
+        }
+        public void RemoveByCode(int value)
+        {
+            foreach(Book book in Books)
+            {
+                if (book.No == value)
+                {
+                    Books.Remove(book);
+                }
+            }
+        }
+    }
+}
