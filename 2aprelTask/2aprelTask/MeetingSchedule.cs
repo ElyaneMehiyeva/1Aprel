c29a2a0ae50ceb5d70a5c84cfefbf6ffbcd70857
Elyane <elyamehiyeva@gmail.com>
2022-04-03 21:20:07 +0400

    2aprel

---
+++ 2aprelTask/2aprelTask/MeetingSchedule.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2aprelTask
+{
+    internal class MeetingSchedule
+    {
+        public List<Meeting> Meetings = new List<Meeting>();
+        public void SetMeeting(string name, DateTime fromDate, DateTime toDate)
+        {
+            Meeting meeting1 = new Meeting()
+            {
+                Name = name,
+                FromDate = fromDate,
+                ToDate = toDate,
+            };
+            if (Meetings.Count == 0)
+            {                
+                Meetings.Add(meeting1);
+            } else
+            {
+                int count = 0;
+                foreach (Meeting Meeting in Meetings)
+                {
+
+                    if (Meeting.FromDate > toDate || Meeting.ToDate < fromDate)
+                    {
+
+                        count = 1;
+
+                    }
+                    else
+                    {
+                        throw new Exception("Bu vaxtda meeting var!");
+                    }
+                }
+                if(count == 1)
+                {
+                    Meetings.Add(meeting1);
+                }
+            }
+            
+        }
+        public int FindMeetingsCount(DateTime time)
+        {
+            List<Meeting> newMeetings = new List<Meeting>();
+            foreach (Meeting Meeting in Meetings)
+            {
+                if (time < Meeting.FromDate)
+                {
+                   newMeetings.Add(Meeting);
+                }
+            }
+            return newMeetings.Count;   
+        }
+        public bool IsExistsMeetingByName(string text)
+        {
+            foreach (Meeting Meeting in Meetings)
+            {
+                if (Meeting.Name.Contains(text))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public List<Meeting> GetExistMeetings(string name)
+        {
+            List<Meeting> newMeetings = new List<Meeting>();
+            foreach (Meeting Meeting in Meetings)
+            {
+                if (Meeting.Name.Contains(name))
+                {
+                    newMeetings.Add(Meeting);
+                }
+            }
+            return newMeetings;
+        }
+    }
+}
---
+++ 2aprelTask/2aprelTask/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _2aprelTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Meeting
+            //Meeting meeting1 = new Meeting()
+            //{
+            //    Name = "1-1 gorusme",
+            //    FromDate = new DateTime(2022, 04, 01, 13, 00, 00),
+            //    ToDate = new DateTime(2022, 04, 01, 14, 00, 00),
+            //};
+            //Meeting meeting2 = new Meeting()
+            //{
+            //    Name = "Rektor ile gorus",
+            //    FromDate = new DateTime(2022, 04, 02, 15, 00, 00),
+            //    ToDate = new DateTime(2022, 04, 02, 18, 00, 00),
+            //};
+            #endregion
+
+            MeetingSchedule schedule1 = new MeetingSchedule();
+            try
+            {
+                schedule1.SetMeeting("meet1", new DateTime(2022, 04, 01, 13, 00, 00), new DateTime(2022, 04, 01, 14, 00, 00));
+                schedule1.SetMeeting("meet2", new DateTime(2022, 04, 02, 15, 00, 00), new DateTime(2022, 04, 02, 18, 00, 00));
+                schedule1.SetMeeting("meet3", new DateTime(2022, 04, 02, 16, 00, 00), new DateTime(2022, 04, 02, 18, 00, 00));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine(schedule1.FindMeetingsCount(new DateTime(2022, 02, 12)));
+            foreach (var item in schedule1.Meetings)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
+
